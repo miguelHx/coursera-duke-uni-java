4338e1542427e824d2065b945314de60bded4fbd
Miguel Hernandez <mhernandez401405@gmail.com>
2016-03-24 20:06:40 -0700

    Create week4exercise3.js

---
+++ programming-web/week4exercise3.js
@@ -0,0 +1,28 @@
+//set black function
+function setBlack(pixel) {
+    pixel.setRed(0);
+    pixel.setGreen(0);
+    pixel.setBlue(0);
+    return pixel;
+}
+
+//pixel on edge function that returns true if the pixel's location is within borderWidth of any of the four borders, and thus on the border. Otherwise, return false.
+function pixelOnEdge(pixel, image, borderWidth) {
+    var x = pixel.getX();
+    var y = pixel.getY();
+    var width = image.getWidth();
+    var height = image.getHeight();
+    if (x < borderWidth || y < borderWidth || x >= width - borderWidth || y >= height - borderWidth) {
+        pixel = setBlack(pixel);
+    }
+}
+var image = new SimpleImage("usain.jpg");
+for (var pixel of image.values()) {
+    if (pixelOnEdge(pixel, image, 60)) {
+    }
+}
+
+
+
+
+print(image);
