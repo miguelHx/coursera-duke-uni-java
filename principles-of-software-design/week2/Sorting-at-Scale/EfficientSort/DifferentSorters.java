b0924c34653ba3b653c9be607f5b029b71e0d87b
Miguel Hernandez <mhernandez401405@gmail.com>
2016-08-01 20:59:27 -0700

    Create DifferentSorters.java

---
+++ principles-of-software-design/week2/Sorting-at-Scale/EfficientSort/DifferentSorters.java
@@ -0,0 +1,91 @@
+
+/**
+ * 
+ * @author Miguel Hernandez
+ * @version nil
+ */
+
+import java.util.*;
+
+public class DifferentSorters {
+    public void sortWithCompareTo() {
+        EarthQuakeParser parser = new EarthQuakeParser();
+        //String source = "data/nov20quakedata.atom";
+        //String source = "data/earthQuakeDataDec6Sample2.atom";
+        String source = "data/earthQuakeDataWeekDec6sample2.atom";
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);
+        Collections.sort(list);
+        /*
+        for(QuakeEntry qe: list) {
+            System.out.println(qe);
+        }
+        */
+        int quakeNumber = 600;
+        System.out.println("Print quake entry in position " + quakeNumber);
+        System.out.println(list.get(quakeNumber));
+    }
+
+    public void sortByMagnitude() {
+        EarthQuakeParser parser = new EarthQuakeParser();
+        String source = "data/nov20quakedata.atom";
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);
+        Collections.sort(list, new MagnitudeComparator());
+        for(QuakeEntry qe: list) {
+            System.out.println(qe);
+        }
+
+    }
+
+    public void sortByDistance() {
+        EarthQuakeParser parser = new EarthQuakeParser();
+        String source = "data/nov20quakedata.atom";
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);
+        // Location is Durham, NC
+        Location where = new Location(35.9886, -78.9072);
+        Collections.sort(list, new DistanceComparator(where));
+        for(QuakeEntry qe: list) {
+            System.out.println(qe);
+        }
+
+    }
+    
+    public void sortByTitleAndDepth() {
+        EarthQuakeParser parser = new EarthQuakeParser();
+        //String source = "data/nov20quakedata.atom";
+        //String source = "data/earthQuakeDataDec6Sample2.atom";
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        String source = "data/earthQuakeDataWeekDec6sample1.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);
+        Collections.sort(list, new TitleAndDepthComparator());
+        /*
+        for(QuakeEntry qe: list) {
+            System.out.println(qe);
+        }
+        */
+        int quakeNumber = 500;
+        System.out.println("Print quake entry in position " + quakeNumber);
+        System.out.println(list.get(quakeNumber));
+        
+    }
+    
+    public void sortByLastWordInTitleThenByMagnitude() {
+        EarthQuakeParser parser = new EarthQuakeParser();
+        //String source = "data/nov20quakedata.atom";
+        //String source = "data/earthQuakeDataDec6Sample2.atom";
+        String source = "data/earthQuakeDataWeekDec6sample2.atom";
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);
+        Collections.sort(list, new TitleLastAndMagnitudeComparator());
+        /*
+        for(QuakeEntry qe: list) {
+            System.out.println(qe);
+        }
+        */
+        int quakeNumber = 500;
+        System.out.println("Print quake entry in position " + quakeNumber);
+        System.out.println(list.get(quakeNumber));
+    }
+}
